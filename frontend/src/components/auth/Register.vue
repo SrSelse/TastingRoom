56e4291898cc37c295a559c146ce1ace68a67563
Anders Stigsson <anders@smoothcomp.com>
2026-01-31 14:03:35 +0100

    Spinner and disabled when registering to avoid multiples. Regex sanity check

--- frontend/src/components/auth/Register.vue
+++ frontend/src/components/auth/Register.vue
@@ -50,10 +50,33 @@
 
       <div>
         <button
-          @click="handleRegister"
           class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 focus:ring-offset-gray-900"
+          @click="handleRegister"
         >
           Sign up
+          <svg 
+            v-if="registerInProgress"
+            class="mr-3 ml-1 size-5 animate-spin text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle 
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            >
+            </circle>
+            <path 
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            >
+            </path>
+          </svg>
         </button>
       </div>
       <div
@@ -74,13 +97,22 @@ import { useRouter } from 'vue-router';
 const username = ref('');
 const password = ref('');
 const confirmPassword = ref('');
+const registerInProgress = ref(false);
 const error = ref('');
 const router = useRouter();
 
 const handleRegister = async () => {
+  if (registerInProgress.value) {
+    return;
+  }
+
   if (username.value === '' || password.value === '' || confirmPassword.value === '') {
     error.value = 'Please fill in all fields';
     return;
+  }
+  if (/^[a-zA-Z0-9_-]+$/.test(username.value) === false) {
+    error.value = 'Username must only contain A-Z, a-z, 0-9, _ and -'
+    return
   }
   if (password.value !== confirmPassword.value) {
     error.value = 'Passwords do not match!';
@@ -93,6 +125,7 @@ const handleRegister = async () => {
   }
 
   try {
+    registerInProgress.value = true;
     let resp = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
       method: 'POST',
       body: JSON.stringify({
@@ -132,6 +165,8 @@ const handleRegister = async () => {
   } catch (e) {
     error.value = e;
     console.error(e);
+  } finally {
+      registerInProgress.value = false;
   }
 };
 </script>
